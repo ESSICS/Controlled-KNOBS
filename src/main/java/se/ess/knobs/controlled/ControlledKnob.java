d1b049244a87a07fb5257597b826bc2c858b1475
Claudio Rosati <Claudio.Rosati@esss.se>
2017-08-23 15:39:09 +0200

    Initial commit.

---
+++ src/main/java/se/ess/knobs/controlled/ControlledKnob.java
@@ -0,0 +1,32 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * Copyright (C) 2017 by European Spallation Source ERIC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.ess.knobs.controlled;
+
+
+import java.util.logging.Logger;
+import se.ess.knobs.Knob;
+
+
+/**
+ * @author Claudio Rosati, European Spallation Source ERIC
+ * @version 1.0.0 23 Aug 2017
+ */
+public class ControlledKnob extends Knob {
+
+    private static final Logger LOGGER = Logger.getLogger(ControlledKnob.class.getName());
+
+}
